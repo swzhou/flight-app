5bfada39eba9f4b4ef0b6e44ca3dd60960e11fee
swzhou <swing.zhou@hotmail.com>
2012-06-02 18:07:13 +0800

    add domain Flight and FlightDescription

---
+++ src/main/java/sample/roo/flightapp/domain/FlightDescription.java
@@ -0,0 +1,17 @@
+package sample.roo.flightapp.domain;
+
+import org.springframework.roo.addon.equals.RooEquals;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.serializable.RooSerializable;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEquals
+@RooSerializable
+public class FlightDescription {
+
+    private String fromCity;
+
+    private String toCity;
+}
