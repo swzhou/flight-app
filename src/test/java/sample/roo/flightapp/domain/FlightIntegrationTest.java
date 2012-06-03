b92393f7a1ccaea9d2ed7d4b400d845f0a5693cb
swzhou <swing.zhou@hotmail.com>
2012-06-03 10:30:00 +0800

    create custom data on demand and use in test case

--- src/test/java/sample/roo/flightapp/domain/FlightIntegrationTest.java
+++ src/test/java/sample/roo/flightapp/domain/FlightIntegrationTest.java
@@ -1,12 +1,22 @@
 package sample.roo.flightapp.domain;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
 @RooIntegrationTest(entity = Flight.class)
 public class FlightIntegrationTest {
 
+	@Autowired
+	private MyFlightDod myDod;
+	
     @Test
     public void testMarkerMethod() {
+    }
+    
+    @Test
+    public void testMyCustomDod() {
+    	Flight flight = myDod.getRandomFlight();
+    	System.out.println(flight.getOrigin());
     }
 }
---
+++ src/test/java/sample/roo/flightapp/domain/MyFlightDod.java
@@ -0,0 +1,7 @@
+package sample.roo.flightapp.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Flight.class)
+public class MyFlightDod {
+}
